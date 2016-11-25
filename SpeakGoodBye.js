ca640c22aa6edc0697a77a7ef51c676b66630be4
ruyang <ruyang@ea.com>
2016-11-24 17:14:38 -0800

    remove  function name

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -17,7 +17,7 @@
   // STEP 8: Rewrite the 'speak' function such that it is attached to the
   // byeSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  byeSpeaker.speak = function speak(name) {
+  byeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   };
 
